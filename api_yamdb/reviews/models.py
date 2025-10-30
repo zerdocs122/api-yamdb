dfe087e63900a977f87b657530ccc7d37a685654
unknown <dogsig22@gmail.com>
2025-10-30 22:41:13 +0500

    Fix BUGS

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -113,4 +113,3 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
-from django.core.exceptions import ValidationError
