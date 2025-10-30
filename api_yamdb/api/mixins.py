6c68100687196daf3256602c66cf6fcd0f06a89e
Andrey Melnik <cragwer476@gmail.com>
2025-10-30 17:39:34 +0300

    Fix titles bugs

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -2,12 +2,15 @@ from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework import filters
 
+from .permissions import IsAdminOrReadOnly
+
 
 class ListCreateDeleteViewSet(
     mixins.ListModelMixin, mixins.CreateModelMixin,
     mixins.DestroyModelMixin, viewsets.GenericViewSet
 ):
     filter_backends = (filters.SearchFilter,)
+    permission_classes = [IsAdminOrReadOnly]
     search_fields = ('name',)
     lookup_field = 'slug'
     lookup_url_kwarg = 'slug'
