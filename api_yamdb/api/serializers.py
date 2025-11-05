1312e9253cce1a63d6db32971729ee97671731cd
Kutov Danil <kutov.danil@yandex.ru>
2025-11-05 16:44:39 +0300

    remove unusable import

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
